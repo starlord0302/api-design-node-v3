134f8ec0b87e5295732687bbe95537e508a49085
Károly Kovács <karoly0302@gmail.com>
2019-11-19 14:53:55 +0100

    feat(): lesson 1 done

--- src/server.js
+++ src/server.js
@@ -12,4 +12,17 @@ app.use(json())
 app.use(urlencoded({ extended: true }))
 app.use(morgan('dev'))
 
-export const start = () => {}
+app.get('/', (req, res) => {
+  res.send({ data: 'Hello World!' })
+})
+
+app.post('/', (req, res) => {
+  console.log(req.body)
+  res.send({ message: 'ok' })
+})
+
+export const start = () => {
+  app.listen(3000, () => {
+    console.log('Server is listening on port 3000')
+  })
+}
